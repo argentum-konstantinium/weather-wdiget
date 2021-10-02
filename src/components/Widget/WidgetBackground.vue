0711f50ecc7179c9329d0903b4963e5b548c12c9
Konstantin <argentum.konstantinium@gmail.com>
2021-10-02 04:11:42 +0300

    feat: create widget background

---
+++ src/components/Widget/WidgetBackground.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="widget-background" aria-hidden="true"></div>
+</template>
+
+<script>
+export default {
+  name: 'WidgetBackground',
+};
+</script>
+
+<style lang="scss" scoped>
+.widget-background {
+  position: absolute;
+  background: rgba(159, 159, 229, 0.68);
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  z-index: -1;
+  pointer-events: none;
+  backdrop-filter: blur(4px);
+}
+</style>
