a8ee2021b9afa58ca79ed2cbdc56c2753e01800a
Konstantin <argentum.konstantinium@gmail.com>
2021-10-03 21:40:24 +0300

    feat: create metcasts component

---
+++ src/components/Widget/WidgetMetcasts.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-if="metcasts.length > 0" class="widget-body">
+    <BasicCarousel>
+      <WidgetMetcast v-for="metcast of metcasts" :key="metcast.name"
+                     :metcast="metcast"/>
+    </BasicCarousel>
+  </div>
+</template>
+
+<script>
+import WidgetMetcast from './WidgetMetcast.vue';
+import BasicCarousel from '@/components/basic/BasicCarousel.vue';
+import metcastsMixin from '@/mixins/metcasts';
+import locationsMixin from '@/mixins/locations';
+
+export default {
+  name: 'WidgetMetcasts',
+  components: {
+    BasicCarousel,
+    WidgetMetcast,
+  },
+  mixins: [metcastsMixin, locationsMixin],
+};
+</script>
